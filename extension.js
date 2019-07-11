9e2d8c93e3ad851f950db336c6f878cc9e745e44
eyea <deyesea@gmail.com>
2019-07-11 23:20:28 +0800

    vscode hasn't supported preview audio files , so  play bilibili stop for a while

--- extension.js
+++ extension.js
@@ -17,11 +17,24 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('extension.sb', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('everything will be ok ');
+
+		// start my word
+		vscode.window.createWebviewPanel({
+			viewType: 'markdown.preview',
+			title: 'see films  ',
+			showOptions: {
+				preserveFocus: true,
+				// viewColumn:
+			}
+		})
+		// ❌ vscode  hasn't supported preview audio files ... haha ❌
+
+		// the end of my work
 	});
 
 	context.subscriptions.push(disposable);
